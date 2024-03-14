e47e358fd289db869d6b74fe7e054e71f8535fe0
Hanna_Urban <huan92@yandex.ru>
2024-03-15 01:20:22 +0300

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,18 +4,23 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        try {boolean result = true;
+        boolean result = true;
+        if (args == null || args.isEmpty()){
+            return false;
+        }
         for (String elem : args) {
-            double number = Double.parseDouble(elem);
-            if (number<=0)
-        {result = false;
-            break;
-            }}
-       return result;}
-        catch (NumberFormatException e | NullPointerException e)
-        {return false;}
+            try {
+                double number = Double.parseDouble(elem);
+                if (Double.isNaN(number) || number <= 0) {
+                    result = false;
+                    break;
+                } }
+            catch (NumberFormatException | NullPointerException e){
+            result = false;
+        }
     }
-}
+    return result;
+}}
 
 
 
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,13 +2,17 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str == null){
+return false;
+}
          try {double number = Double.parseDouble(str);
-             if (number>0 && Double.isNaN(number))
+             if (number>0 && !Double.isNaN(number))
              {return true;}
              else {return false;}
 
          }
-         catch (NumberFormatException e | NullPointerException e)
+         catch (NumberFormatException | NullPointerException e)
          {return false;}
     }
 }
+
