d135e268a469461802089a0331542a5f757d527a
Hanna_Urban <huan92@yandex.ru>
2024-03-15 00:25:41 +0300

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,19 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        boolean result = true;
+        for (String elem : args) {
+            int number = Integer.parseInt(elem);
+            if (number<=0)
+        {result = false;
+            break;
+            }}
+       return result;
     }
 }
+
+
+
+
+
+
