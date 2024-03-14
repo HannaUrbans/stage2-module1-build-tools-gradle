288d277f603adda23b864978e797fb4a54f8e77e
Hanna_Urban <huan92@yandex.ru>
2024-03-15 00:45:11 +0300

    done

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,7 +3,7 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          try {double number = Double.parseDouble(str);
-             if (number>0)
+             if (number>0 && !Double.isNaN(number))
              {return true;}
              else {return false;}
 
